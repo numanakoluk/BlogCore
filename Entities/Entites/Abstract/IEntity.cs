011d6f79b9a9ae3f81d70c312c858e3c28575e24
numanakoluk <numanakoluk01@gmail.com>
2022-03-09 17:48:56 +0300

    Edit

---
+++ Entities/Entites/Abstract/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entites.Abstract
+{
+    //Repository Pattern için.
+    public interface IEntity
+    {
+        s
+    }
+}
