f97ad5eb4f83106854064bc1ed693aa19d0eb5a9
numanakoluk <numanakoluk01@gmail.com>
2022-03-10 01:55:09 +0300

    ArticleClass

---
+++ Entities/Concrete/Article.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Thumbnail { get; set; }
+        public DateTime Date { get; set; }
+        public int ViewsCount { get; set; }
+        public int CommentCount { get; set; }
+        public string SeaAuthor { get; set; } //AramaMotoru için.MetaTag.Paylaşan.
+
+        public string SeaDescription { get; set; }
+        public string SeaText { get; set; } //text header
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
