140785880aff4e31077c6bcb316b885c37872df5
numanakoluk <numanakoluk01@gmail.com>
2022-03-09 13:24:43 +0300

    EntityBase

---
+++ Entities/Entites/Abstract/EntityBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entites.Abstract
+{
+    //Polymorphism için
+    public abstract class EntityBase
+    {
+
+        public virtual int Id { get; set; }
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public virtual DateTime ModifiedDateTime { get; set; } = DateTime.Now;
+
+        public virtual bool IsDeleted { get; set; } = false; //silindi mi
+
+        public virtual bool IsActive { get; set; } = true;
+
+        public virtual string  CreatedByName { get; set; }
+
+        public virtual string  ModifiedByName { get; set; }
+
+    }
+}
