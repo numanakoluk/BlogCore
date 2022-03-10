3bd5a67fdf3b71e645ebff0acf6fd53d2d8fa972
numanakoluk <numanakoluk01@gmail.com>
2022-03-10 14:03:24 +0300

    IEntityRepository

--- Shared/Data/Abstract/IEntityRepository.cs
+++ Shared/Data/Abstract/IEntityRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Entites.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -7,9 +8,23 @@ using System.Threading.Tasks;
 
 namespace Shared.Data.Abstract
 {
-    public interface IEntityRepository<T>
+    public interface IEntityRepository<T> where T : class, IEntity, new()
     {
         //Predicate filtre params ile birden fazla parametre alabilir.Kullanıcıyı getirdik.Kullanıcın makalelerini de getir derken params ve bir dizi yoladım.Arrayismi includeProperties.
         Task<T> GetAsync(Expression<Func<T,bool>> predicate, params Expression<Func<T,object>>[] includeProperties);
+
+        //Birden çok bir listeye ihtiyacımız olursa.Predicate null ise tüm article gelsin gelmzse filtreye göre yüklüyor olucaz.
+        Task<IList<T>> GetAllAsync(Expression<Func<T,bool>> predicate=null, params Expression<Func<T,object>> [] includeProperties);
+
+        Task AddAsync(T entity);
+
+        Task UpdateAsync(T entiy);
+
+        Task DeleteAsync(T entity);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+
+        Task<int> CountAsync(Expression<Func<T, bool>> predicate);
+
     }
 }
