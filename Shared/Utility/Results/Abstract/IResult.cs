ae63c9a0f7397c2d9bec6ab3d30382b548d09f0f
numanakoluk <numanakoluk01@gmail.com>
2022-03-15 13:16:17 +0300

    ResultStatus IResult

---
+++ Shared/Utility/Results/Abstract/IResult.cs
@@ -0,0 +1,20 @@
+﻿using Shared.Utility.Results.Complex_Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Utility.Results.Abstract
+{
+    interface IResult
+    {
+        //Değiştirilebilir değil bu yüzden set yok.
+        public ResultStatus ResultStatus { get; } //ResultStatus.Success//ResultStatus.Error
+
+        public string Message { get;}
+
+        //Exception 
+        public Exception Exception { get; }
+    }
+}
