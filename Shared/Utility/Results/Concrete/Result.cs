b9a57f6d334bf7b1478bffc1f13345e2357d3be1
numanakoluk <numanakoluk01@gmail.com>
2022-03-16 12:46:21 +0300

    ResultClass

---
+++ Shared/Utility/Results/Concrete/Result.cs
@@ -0,0 +1,40 @@
+﻿using Shared.Utility.Results.Abstract;
+using Shared.Utility.Results.Complex_Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Utility.Results.Concrete
+{
+    public class Result : IResult
+    {
+        public Result(ResultStatus resultStatus)
+        {
+            ResultStatus = resultStatus;
+        }
+
+        //One more than ctor
+        public Result(ResultStatus resultStatus, string message)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+        }
+
+        public Result(ResultStatus resultStatus, string message, Exception exception)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Exception = exception;
+        }
+
+        public ResultStatus ResultStatus { get; }
+
+        public string Message { get; }
+
+        public Exception Exception { get; }
+
+        // new Result(ResultStatus.Error,"Proccess not continuted", Exception)
+    }
+}
