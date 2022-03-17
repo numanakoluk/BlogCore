9602529aa6d47a87e5227f77c3436b7cd9bb46cf
numanakoluk <numanakoluk01@gmail.com>
2022-03-17 12:57:04 +0300

    CategoryUpdateDto

--- Entities/Dtos/CategoryAddDto.cs
+++ Entities/Dtos/CategoryAddDto.cs
@@ -12,23 +12,23 @@ namespace Entities.Dtos
     {
 
         [DisplayName("Kategori Adı")]
-        [Required(ErrorMessage ="{0} Boş Geçilemez.")]
-        [MaxLength(70,ErrorMessage ="{0} {1} karakterden büyük olamaz.")]
+        [Required(ErrorMessage ="{0} Boş geçilmemelidir.")]
+        [MaxLength(70,ErrorMessage ="{0} {1} karakterden büyük olmamalıdır..")]
         [MinLength(3, ErrorMessage ="{0} {1} karekterden az olmamalıdır.")]
         public string Name { get; set; }
 
         [DisplayName("Kategori Açıklaması")]
-        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olamaz.")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
         [MinLength(3, ErrorMessage = "{0} {1} karekterden az olmamalıdır.")]
         public string Description { get; set; }
 
         [DisplayName("Kategori Özel Not Alanı")]
-        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olamaz.")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
         [MinLength(3, ErrorMessage = "{0} {1} karekterden az olmamalıdır.")]
         public string Note { get; set; }
 
         [DisplayName("Aktif Mi?")]
-        [Required(ErrorMessage = "{0} Boş Geçilemez.")]
+        [Required(ErrorMessage = "{0} Boş geçilmemelidir.")]
 
         public bool IsActive { get; set; }
 
