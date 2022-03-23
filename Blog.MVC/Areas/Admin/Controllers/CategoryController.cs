9041f7f4eecc322ad09514349d8a749a29bec1d5
numanakoluk <numanakoluk01@gmail.com>
2022-03-23 20:43:26 +0300

    GetListCategory and View List

---
+++ Blog.MVC/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Abstract;
+using Shared.Utility.Results.Complex_Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.MVC.Controllers.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _categoryService.GetAll();
+            if (result.ResultStatus==ResultStatus.Success)
+            {
+                return View(result.Data);
+            }
+
+            return View();
+        }
+    }
+}
